b5b438a01a5d985947244e4efec306bbd634dbd8
Carmen Maria <carsoto8691@gmail.com>
2020-04-17 16:26:53 -0500

    Corrección de error en los dashboard client y customer

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -11,6 +14,14 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\User::class, 20)->create();
+    	$user = new User();
+        $user->name = 'Admin';
+        $user->email = 'admin@example.com';
+        $user->email_verified_at = now();
+        $user->password = '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi';
+        $user->remember_token = Str::random(10);
+        $user->save();
+
+        factory(User::class, 20)->create();
     }
 }
